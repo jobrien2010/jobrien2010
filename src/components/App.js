77c255daf97f48f8496e7d8c7cfaadc44615aba0
James O'Brien <gettysburg61@gmail.com>
2021-07-08 11:52:45 -0400

    comment out class-based App compoonent

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import SearchBar from './searchbar';
 import VideoList from './videolist';
 import VideoDetail from './videodetail';
 import useVideos from '../hooks/useVideos';
+//import youtube from '../apis/youtube';
 
 
 
@@ -33,63 +34,59 @@ const App = () => {
             </div>
         </div>
     );
-
-
-
 };
 
 
-//Class-based component
-class _App extends React.Component {
+// //Class-based component
+// class _App extends React.Component {
 
-    state = { 
-        videos: [],
-        selectedVideo: null
-     };
+//     state = { 
+//         videos: [],
+//         selectedVideo: null
+//      };
 
-     onVideoSelect = (video) => {
-         this.setState({
-             selectedVideo: video,
-            });
-     }
+//      onVideoSelect = (video) => {
+//          this.setState({
+//              selectedVideo: video,
+//             });
+//      }
 
-     componentDidMount(){
-         this.onSearchSubmit('buildings');
-     }
+//      componentDidMount(){
+//          this.onSearchSubmit('buildings');
+//      }
 
-    onSearchSubmit = async (term) => {
-        this.setState({selectedVideo: null});
-        const response = await youtube.get(
-            '/search',
-            {
-                params:{ q: term }
-            });
-            this.setState({
-                videos: response.data.items,
-                selectedVideo: response.data.items[0]
-            });
+//     onSearchSubmit = async (term) => {
+//         this.setState({selectedVideo: null});
+//         const response = await youtube.get(
+//             '/search',
+//             {
+//                 params:{ q: term }
+//             });
+//             this.setState({
+//                 videos: response.data.items,
+//                 selectedVideo: response.data.items[0]
+//             });
 
-    }
+//     }
 
-
-    render(){
-        return(
+//     render(){
+//         return(
             
-            <div className="ui container" style={{marginTop: '10px'}}>
-                <SearchBar onFormSubmit={this.onSearchSubmit}/>
-                <div className="ui grid">
-                    <div className="ui row">
-                        <div className="eleven wide column">
-                            <VideoDetail video={this.state.selectedVideo}/>
-                        </div>
-                        <div className="five wide column">
-                            <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+//             <div className="ui container" style={{marginTop: '10px'}}>
+//                 <SearchBar onFormSubmit={this.onSearchSubmit}/>
+//                 <div className="ui grid">
+//                     <div className="ui row">
+//                         <div className="eleven wide column">
+//                             <VideoDetail video={this.state.selectedVideo}/>
+//                         </div>
+//                         <div className="five wide column">
+//                             <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
 }
 
 export default App;
